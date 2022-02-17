d2181d0b88d90b78cf6f657b1499ef8be2ea2ca2
Anastasia <anastasia.brinati@students.uniroma2.eu>
2022-02-17 11:48:16 +0100

    sonar

--- src/main/java/com/example/calcolatorebinario/BinaryCalculator.java
+++ src/main/java/com/example/calcolatorebinario/BinaryCalculator.java
@@ -9,7 +9,7 @@ public class BinaryCalculator {
         }
 
         int len = number1.length();
-        String result = "";
+        StringBuilder result = new StringBuilder();
         int reminder = 0;
 
         if(len != number2.length()){
@@ -22,17 +22,17 @@ public class BinaryCalculator {
             if(number1.charAt(i) == 0){
                 if(number2.charAt(i) == 0) {
                     if (reminder == 1) {
-                        result = result + "1";
+                        result.append(result + "1");
                     } else {
-                        result = result + "0";
+                        result.append(result + "0");
                     }
                     reminder = 0;
                 }else{
                     if (reminder == 1) {
-                        result = result + "0";
+                        result.append(result + "0");
                         reminder = 1;
                     } else {
-                        result = result + "1";
+                        result.append(result + "1");
                         result.chars();
                         reminder = 0;
                     }
@@ -42,17 +42,17 @@ public class BinaryCalculator {
             else{
                 if(number2.charAt(i) == 0){
                     if (reminder == 1) {
-                        result = result + "0";
+                        result.append(result + "0");
                         reminder = 1;
                     } else {
-                        result = result + "1";
+                        result.append(result + "1");
                         reminder = 0;
                     }
                 }else{
                     if (reminder == 1) {
-                        result = result + "1";
+                        result.append(result + "1");
                     } else {
-                        result = result + "0";
+                        result.append(result + "0");
                     }
                     reminder = 1;
                 }
@@ -61,10 +61,10 @@ public class BinaryCalculator {
         }
 
         if(reminder == 1){
-            result = "1" + result;
+            result.append(result + "1");
         }
 
-        return result;
+        return result.toString();
 
     }
 
